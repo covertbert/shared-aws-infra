a75e03a52c46698786c83208d8bb64f1b7780485
Bertie Blackman <blackmanrgh@gmail.com>
2020-09-25 14:00:19 +0100

    Adds S3 bucket for website

---
+++ lib/s3.ts
@@ -0,0 +1,13 @@
+import { Stack, App, StackProps, Duration } from '@aws-cdk/core'
+import { Bucket } from '@aws-cdk/aws-s3'
+
+export class S3Stack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
+    super(scope, id, props)
+
+    new Bucket(this, 'BertieBlackmanBucket', {
+      bucketName: 'BertieBlackmanBucket',
+      lifecycleRules: [{ expiration: Duration.days(30) }],
+    })
+  }
+}
