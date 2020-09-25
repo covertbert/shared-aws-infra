31f7ca40a39863418a36c0b3c51d844cc5f5a273
Bertie Blackman <blackmanrgh@gmail.com>
2020-09-25 17:51:32 +0100

    Adds cf aliases and certificate to bertie-blackman cf distro

--- lib/static-site.ts
+++ lib/static-site.ts
@@ -26,6 +26,11 @@ export class StaticSiteStack extends Stack {
     })
 
     const cloudfrontDistProps: CloudFrontWebDistributionProps = {
+      aliasConfiguration: {
+        acmCertRef:
+          'arn:aws:acm:us-east-1:515213366596:certificate/904b7400-ca9a-4f45-8f77-91deccfd79c1',
+        names: ['bertie.dev', 'www.bertie.dev'],
+      },
       originConfigs: [
         {
           s3OriginSource: {
