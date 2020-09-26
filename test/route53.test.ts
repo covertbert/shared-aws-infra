2f3e4be648f9955e405e6db41c65befd3acf14f2
Bertie Blackman <blackmanrgh@gmail.com>
2020-09-26 20:42:36 +0100

    Adds Route53 tests

---
+++ test/route53.test.ts
@@ -0,0 +1,17 @@
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import { Route53Stack } from '../lib'
+
+describe('Route53Stack', () => {
+  it('creates the correct CloudFormation template', () => {
+    const app = new cdk.App()
+
+    const stack = new Route53Stack(app, 'route-53')
+
+    expectCDK(stack).to(
+      haveResource('AWS::Route53::HostedZone', {
+        Name: 'bertie.dev.',
+      }),
+    )
+  })
+})
