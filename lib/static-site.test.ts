71ea99e72fae0205d5fd5e819640a6abb2581aa5
Bertie Blackman <blackmanrgh@gmail.com>
2020-09-29 10:11:27 +0100

    Adds ability to pass certificate ARN to static site

--- lib/static-site.test.ts
+++ lib/static-site.test.ts
@@ -9,6 +9,8 @@ describe('StaticSiteStack', () => {
     const stack = new StaticSiteStack(app, 'test-site', {
       recordName: 'www',
       domainName: 'example.dev',
+      certificateARN:
+        'arn:aws:acm:us-east-1:515213366596:certificate/904b7400-ca9a-4f45-8f77-91deccfd79c1',
     })
 
     expectCDK(stack).to(
