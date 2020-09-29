1bff857aec29ecf6f8db9dffbe9c484087ee594c
Bertie Blackman <blackmanrgh@gmail.com>
2020-09-29 09:53:30 +0100

    Moves Route 53 config into static site stack

--- bin/main.ts
+++ bin/main.ts
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
-import {
-  BudgetsStack,
-  IAMStack,
-  StaticSiteStack,
-  CertificatesStack,
-  Route53Stack,
-  SESStack,
-} from '../lib'
+import { BudgetsStack, IAMStack, StaticSiteStack, CertificatesStack, SESStack } from '../lib'
 
 const regions = {
   primary: 'eu-west-2',
@@ -20,6 +13,9 @@ const app = new cdk.App()
 new BudgetsStack(app, 'budgets', { env: { region: regions.primary } })
 new IAMStack(app, 'iam', { env: { region: regions.primary } })
 new CertificatesStack(app, 'certificates', { env: { region: regions.secondary } })
-new StaticSiteStack(app, 'bertie-blackman', { env: { region: regions.primary } })
-new Route53Stack(app, 'route-53', { env: { region: regions.primary } })
+new StaticSiteStack(app, 'bertie-blackman', {
+  env: { region: regions.primary },
+  recordName: 'www',
+  domainName: 'bertie.dev',
+})
 new SESStack(app, 'bertie-blackman-ses', { env: { region: regions.secondary } })
--- lib/static-site.ts
+++ lib/static-site.ts
@@ -6,16 +6,24 @@ import {
   CloudFrontWebDistribution,
 } from '@aws-cdk/aws-cloudfront'
 import { PolicyStatement, CanonicalUserPrincipal } from '@aws-cdk/aws-iam'
+import { ARecord, RecordTarget, PublicHostedZone } from '@aws-cdk/aws-route53'
+import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets'
 
+type ExtendedStackProps = StackProps & {
+  recordName: string
+  domainName: string
+}
 export class StaticSiteStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props: ExtendedStackProps) {
     super(scope, id, props)
 
     const WEBSITE_NAME = id
-    const BUCKET_NAME = `${id}-artifacts`
+    const ARTIFACT_BUCKET_NAME = `${WEBSITE_NAME}-artifacts`
+    const { recordName, domainName } = props
+    const fullApexDomain = [recordName, domainName].join('.')
 
-    const bucket = new Bucket(this, BUCKET_NAME, {
-      bucketName: BUCKET_NAME,
+    const websiteBucket = new Bucket(this, ARTIFACT_BUCKET_NAME, {
+      bucketName: ARTIFACT_BUCKET_NAME,
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'index.html',
       removalPolicy: RemovalPolicy.DESTROY,
@@ -29,12 +37,12 @@ export class StaticSiteStack extends Stack {
       aliasConfiguration: {
         acmCertRef:
           'arn:aws:acm:us-east-1:515213366596:certificate/904b7400-ca9a-4f45-8f77-91deccfd79c1',
-        names: ['bertie.dev', 'www.bertie.dev'],
+        names: [domainName, fullApexDomain],
       },
       originConfigs: [
         {
           s3OriginSource: {
-            s3BucketSource: bucket,
+            s3BucketSource: websiteBucket,
             originAccessIdentity: cloudfrontOAI,
           },
           behaviors: [{ isDefaultBehavior: true }],
@@ -48,15 +56,29 @@ export class StaticSiteStack extends Stack {
       cloudfrontDistProps,
     )
 
+    const hostedZone = new PublicHostedZone(this, 'BertieDevZone', { zoneName: domainName })
+
+    new ARecord(this, 'WWWBertieDevARecord', {
+      target: RecordTarget.fromAlias(new CloudFrontTarget(cloudfrontDist)),
+      zone: hostedZone,
+      recordName: fullApexDomain,
+    })
+
+    new ARecord(this, 'NakedBertieDevARecord', {
+      target: RecordTarget.fromAlias(new CloudFrontTarget(cloudfrontDist)),
+      zone: hostedZone,
+      recordName: domainName,
+    })
+
     const cloudfrontS3Access = new PolicyStatement({
       actions: ['s3:GetBucket*', 's3:GetObject*', 's3:List*'],
-      resources: [bucket.bucketArn, `${bucket.bucketArn}/*`],
+      resources: [websiteBucket.bucketArn, `${websiteBucket.bucketArn}/*`],
       principals: [
         new CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId),
       ],
     })
 
-    bucket.addToResourcePolicy(cloudfrontS3Access)
+    websiteBucket.addToResourcePolicy(cloudfrontS3Access)
 
     new CfnOutput(this, 'cloudfront-url', {
       exportName: 'CloudfrontURL',
