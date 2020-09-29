5f1dfdd998c9477d89e97b253c0684cb0512987d
Bertie Blackman <blackmanrgh@gmail.com>
2020-09-29 16:56:52 +0100

    Sets up email forwarding with SES & SNS topic (#21)

--- lib/ses.ts
+++ lib/ses.ts
@@ -1,13 +1,21 @@
 import { Stack, App, StackProps } from '@aws-cdk/core'
 import { ReceiptRuleSet } from '@aws-cdk/aws-ses'
-import { S3 } from '@aws-cdk/aws-ses-actions'
+import { S3, Sns, EmailEncoding } from '@aws-cdk/aws-ses-actions'
 import { Bucket } from '@aws-cdk/aws-s3'
+import { Topic } from '@aws-cdk/aws-sns'
+import { EmailSubscription } from '@aws-cdk/aws-sns-subscriptions'
 
 export class SESStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props)
 
     const BUCKET_NAME = `${id}-emails`
+
+    const emailForwardingTopic = new Topic(this, 'EmailForwarding', {
+      displayName: 'EmailForwarding',
+      topicName: 'bertie-blackman-email-forwarding',
+    })
+    emailForwardingTopic.addSubscription(new EmailSubscription('blackmanrgh@gmail.com'))
 
     const bucket = new Bucket(this, 'SESBucket', {
       bucketName: BUCKET_NAME,
@@ -24,6 +32,7 @@ export class SESStack extends Stack {
               bucket,
               objectKeyPrefix: 'emails/',
             }),
+            new Sns({ encoding: EmailEncoding.UTF8, topic: emailForwardingTopic }),
           ],
         },
       ],
