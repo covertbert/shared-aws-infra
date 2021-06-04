7cbed918d94f456f7b10275dc550c9cb4aecf773
Bertie Blackman <blackmanrgh@gmail.com>
2021-06-04 17:22:15 +0100

    Splitting out DNS

--- lib/stacks/static-site.test.ts
+++ lib/stacks/static-site.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, haveResource, haveResourceLike } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
-import { StaticSiteStack } from './'
+import { StaticSiteStack, SharedDnsStack } from './'
 
 describe('StaticSiteStack', () => {
   const stackName = 'test-site'
@@ -10,11 +10,19 @@ describe('StaticSiteStack', () => {
 
   const app = new cdk.App()
 
+  const dns = new SharedDnsStack(app, 'TestDNS', {
+    recordName: 'www',
+    domainName: 'example.dev',
+    certificateARN:
+      'arn:aws:acm:us-east-1:515213366596:certificate/904b7400-ca9a-4f45-8f77-91deccfd79c1',
+  })
+
   const stack = new StaticSiteStack(app, stackName, {
     recordName: 'www',
     domainName: 'example.dev',
     certificateARN:
       'arn:aws:acm:us-east-1:515213366596:certificate/904b7400-ca9a-4f45-8f77-91deccfd79c1',
+    hostedZone: dns.hostedZone,
   })
 
   it('creates an S3 bucket with the correct policy', () => {
