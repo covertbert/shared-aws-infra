30ba238a4f36ddda82d75efbdd511620d524ff34
Bertie Blackman <blackmanrgh@gmail.com>
2021-06-07 10:16:59 +0100

    Adds tests around DNS

--- lib/stacks/shared-dns.test.ts
+++ lib/stacks/shared-dns.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert'
+import { expect as expectCDK, haveResource, stringLike } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import { SharedDnsStack } from './shared-dns'
 
@@ -15,7 +15,71 @@ describe('SharedDnsStack', () => {
         Name: `${domainName}.`,
       }),
     )
+  })
 
-    expectCDK(stack).to(haveResource('AWS::Route53::RecordSet'))
+  it('contains an MX record for personal email', () => {
+    expectCDK(stack).to(
+      haveResource('AWS::Route53::RecordSet', {
+        Name: 'bertie.dev.',
+        Type: 'MX',
+        HostedZoneId: {
+          Ref: stringLike('BertieDevZone*'),
+        },
+        ResourceRecords: ['10 in1-smtp.messagingengine.com', '20 in2-smtp.messagingengine.com'],
+        TTL: '1800',
+      }),
+    )
+  })
+
+  it('contains an TXT record for personal email', () => {
+    expectCDK(stack).to(
+      haveResource('AWS::Route53::RecordSet', {
+        Name: 'bertie.dev.',
+        Type: 'TXT',
+        HostedZoneId: {
+          Ref: stringLike('BertieDevZone*'),
+        },
+        ResourceRecords: ['"v=spf1 include:spf.messagingengine.com ?all"'],
+        TTL: '1800',
+      }),
+    )
+  })
+
+  it('contains the CNAME records for personal email', () => {
+    expectCDK(stack).to(
+      haveResource('AWS::Route53::RecordSet', {
+        Name: 'fm1.bertie.dev.dkim.fmhosted.com.bertie.dev.',
+        Type: 'CNAME',
+        HostedZoneId: {
+          Ref: stringLike('BertieDevZone*'),
+        },
+        ResourceRecords: ['bertie.dev'],
+        TTL: '1800',
+      }),
+    )
+
+    expectCDK(stack).to(
+      haveResource('AWS::Route53::RecordSet', {
+        Name: 'fm2.bertie.dev.dkim.fmhosted.com.bertie.dev.',
+        Type: 'CNAME',
+        HostedZoneId: {
+          Ref: stringLike('BertieDevZone*'),
+        },
+        ResourceRecords: ['bertie.dev'],
+        TTL: '1800',
+      }),
+    )
+
+    expectCDK(stack).to(
+      haveResource('AWS::Route53::RecordSet', {
+        Name: 'fm3.bertie.dev.dkim.fmhosted.com.bertie.dev.',
+        Type: 'CNAME',
+        HostedZoneId: {
+          Ref: stringLike('BertieDevZone*'),
+        },
+        ResourceRecords: ['bertie.dev'],
+        TTL: '1800',
+      }),
+    )
   })
 })
